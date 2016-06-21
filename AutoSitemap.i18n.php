ea39b7e1c5c6d0d1a80333782113269b22358bba
Dolfinus <martinov_m_s_@mail.ru>
2016-06-21 15:25:51 +0300

    v1.0

---
+++ AutoSitemap.i18n.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Internationalization file for the GoogleSiteVerification extension.
+*
+* @licence GPL v2 or later
+* @author Karsten Hoffmeyer (Kghbln)
+*/
+
+$messages = array();
+
+/** English
+* @author Adam Shorland
+* @author Dolfinus
+*/
+$messages['en'] = array(
+        'ausmp-desc' => 'Creates and updates a Sitemap file automatically while any site`s page is changing',
+);
+
+/** Russian (Русский)
+* @author Dolfinus
+*/
+$messages['ru'] = array(
+        'ausmp-desc' => 'Автоматически создает и обновляет файл Sitemap при любых правках страниц',
+);
+
