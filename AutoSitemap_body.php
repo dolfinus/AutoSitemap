889f070bf35e8b37ed16a37b9d9114980c83222c
Taylan Kammer <taylan.kammer@gmail.com>
2023-01-31 23:10:55 +0100

    Log warning if sitemap exceeds size limit.

--- AutoSitemap_body.php
+++ AutoSitemap_body.php
@@ -116,19 +116,41 @@ class AutoSitemap {
         $dbr = wfGetDB(DB_REPLICA);
         $res = $dbr->query(self::getSQL());
 
+        // Sitemaps are limited to 50,000 URLs and 50 MB.
+        $entries    = 0;
+        $bytes      = 0;
+        $maxEntries = 50000;
+        $maxBytes   = 50000000;
+
+        /* Note:
+         *
+         * Actually, https://www.sitemaps.org/protocol.html says the limit is 52,428,800 bytes,
+         * which is 50 MiB (MiB = 1024 * 1024), but it uses the old notation of "MB" which has
+         * since been standardized in ISO to mean 1000 * 1000.  We use the lower limit just in
+         * case, because some systems may interpret the "50MB" to mean 50 * 1000 * 1000.
+         *
+         * It's very unlikely to reach the size limit before reaching the 50k URL limit anyway,
+         * unless there are tons of *extremely* long URLs in a sitemap.
+         */
+
         $error = FALSE;
 
         try {
-            self::write($file_handle, $wgAutoSitemap["header"]);
+            $bytes += self::write($file_handle, $wgAutoSitemap["header"]);
             while($row = $res->fetchObject()) {
-                self::write($file_handle, self::formatResult($server, $row));
+                $entries += 1;
+                $bytes += self::write($file_handle, self::formatResult($server, $row));
             }
-            self::write($file_handle, $wgAutoSitemap["footer"]);
+            $bytes += self::write($file_handle, $wgAutoSitemap["footer"]);
         } catch (Exception $e) {
-            error_log("Exception while writing to $tmp_filename: $e");
+            error_log("Writing sitemap failed: $e");
             $error = TRUE;
         } finally {
             fclose($file_handle);
+        }
+
+        if ($entries > $maxEntries || $bytes > $maxBytes) {
+            error_log("WARNING: Sitemap exceeds allowed size limit!");
         }
 
         if ($error) {
@@ -142,10 +164,11 @@ class AutoSitemap {
     }
 
     static function write($handle, $data) {
-        $retval = fwrite($handle, $data);
-        if ($retval === FALSE || $retval === 0) {
-            throw new Exception("fwrite returned $retval");
+        $bytes = fwrite($handle, $data);
+        if ($bytes === FALSE || $bytes === 0) {
+            throw new Exception("Call to fwrite failed (returned $bytes).");
         }
+        return $bytes;
     }
 
     static function getSQL() {
